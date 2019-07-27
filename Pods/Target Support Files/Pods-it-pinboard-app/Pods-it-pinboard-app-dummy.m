a56d9f1f5332f1d417ff12c9713d322c373482d0
Kyrylo Matvieiev <kyrylo.matvieiev@gmail.com>
2019-07-27 17:22:17 +0300

    add template project files

---
+++ Pods/Target Support Files/Pods-it-pinboard-app/Pods-it-pinboard-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_it_pinboard_app : NSObject
+@end
+@implementation PodsDummy_Pods_it_pinboard_app
+@end
---
+++ it-pinboard-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  it-pinboard-app
+//
+//  Created by Kyrylo Matvieiev on 7/27/19.
+//  Copyright © 2019 Kyrylo Matvieiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
