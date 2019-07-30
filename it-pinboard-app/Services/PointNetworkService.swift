e7174fa4571adfcdb091af5c5a2244305de28dae
Kyrylo Matvieiev <kyrylo.matvieiev@gmail.com>
2019-07-30 21:43:37 +0300

    Feature: add sort to get points

--- it-pinboard-app/Services/PointNetworkService.swift
+++ it-pinboard-app/Services/PointNetworkService.swift
@@ -15,6 +15,7 @@ class PointNetworkService {
         static let kPointId = "uid"
         static let kUser = "users"
         static let kPontsList = "pointsList"
+        static let kOrderedByName = "name"
     }
     
     // MARK: - Properties
@@ -26,7 +27,6 @@ class PointNetworkService {
     func addPoint(_ point: Point, to user: User, _ completion: @escaping (Error?) -> Void) {
         let path = ref.child(Keys.kUser).child(user.uid).child(Keys.kPontsList).childByAutoId()
         let fireObj = point.convertToDict()
-        
         path.setValue(fireObj) { (error, ref) in
             if let error = error {
                 completion(error)
@@ -37,19 +37,18 @@ class PointNetworkService {
     }
     
     func getPointLists(for user: User, completion: @escaping ([Point]) -> Void) {
-        let path = ref.child(Keys.kUser).child(user.uid).child(Keys.kPontsList)
-        
+        let path = ref.child(Keys.kUser).child(user.uid).child(Keys.kPontsList).queryOrdered(byChild: Keys.kOrderedByName)
         path.observeSingleEvent(of: .value) { snapshot in
             var tmpPointsList = [Point]()
-            guard let snapshotData = snapshot.value as? [String: [String: Any]] else { return }
-            
-            for snap in snapshotData {
-                guard let point = Point(id: snap.key, dict: snap.value) else { continue }
-                tmpPointsList.append(point)
+            snapshot.children.forEach {
+                if let snapshot = $0 as? DataSnapshot,
+                    let dict = snapshot.value as? [String: Any],
+                    let point = Point(id: snapshot.key, dict: dict) {
+                    tmpPointsList.append(point)
+                }
             }
             completion(tmpPointsList)
         }
-        
     }
     
     func deletePoint(id: String, form user: User, completion: @escaping (Error?) -> Void) {
@@ -62,5 +61,4 @@ class PointNetworkService {
             }
         }
     }
-    
 }
